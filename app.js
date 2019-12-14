36c9d8876f2c1c8307a04056201ae904c5f43d1e
willdk3000 <w.doucetk@gmail.com>
2019-12-14 18:56:46 -0500

    proxy in client package.json

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -17,11 +16,6 @@ require('dotenv').config();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 
@@ -35,9 +29,9 @@ app.use(function (req, res, next) {
 });
 
 // For Heroku - port env variable
-//const PORT = process.env.PORT || 5000
-//app.listen(PORT);
-//console.log("Server listening on port", PORT);
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
+console.log("Server listening on port", PORT);
 
 
 module.exports = app;
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 5000
 
 module.exports = function (app) {
   //app.use(proxy('/auth/google', { target: 'http://localhost:5000' }));
-  app.use(proxy('/api/**', { target: 'http://localhost:' + PORT }));
+  //app.use(proxy('/api/**', { target: `http://localhost:${PORT}` }));
 };

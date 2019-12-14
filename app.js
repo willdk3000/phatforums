c6576e4d713c019760c4082d7569d0527d0d63f2
willdk3000 <w.doucetk@gmail.com>
2019-12-14 18:28:52 -0500

    modified server structure

--- app.js
+++ app.js
@@ -15,24 +15,22 @@ require('dotenv').config();
 //app.use(express.static('client/build'));
 
 if (process.env.NODE_ENV === 'production') {
-
   app.use(express.static('client/build'));
-
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-
 }
-
 
 require('./api/routes')(app);
 
+// catch 404 and forwarding to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // For Heroku - port env variable
-const PORT = process.env.PORT || 5000
-app.listen(PORT);
-console.log("Server listening on port", PORT);
+//const PORT = process.env.PORT || 5000
+//app.listen(PORT);
+//console.log("Server listening on port", PORT);
 
 
 module.exports = app;
